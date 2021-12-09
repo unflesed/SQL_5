71d5f77832db540d9cbaa7d1b4487948e58ed869
Anatoliy Morozov <anatoly.marozaw@gmail.com>
2021-12-09 11:04:09 +0300

    update additional

--- additional.sql
+++ additional.sql
@@ -1,13 +1,19 @@
 use ShopDB;
 
-select e.FName, c.FName, totalPrice  from orders as o
+-- Joins
+select e.FName as Employees, c.FName as Customers, sum(totalprice) as TP
+from orders as o
 join orderdetails as od on od.orderID = o.OrderID
 Join customers as c on o.CustomerNo = c.customerNo
 Join employees as e on o.EmployeeID = e.EmployeeID
-where totalprice > 1000;
+group by customers
+having TP > 1000;
 
+-- Вложенные запросы
 select 	(select FName from customers as c where c.CustomerNo = o.CustomerNo) as Customer,
-		(select FName from employees as e where o.EmployeeID = e.EmployeeID) as Employee
+		(select FName from employees as e where o.EmployeeID = e.EmployeeID) as Employee,
+        sum(TotalPrice) as TP
 from orders as o
 join orderdetails as od on od.orderID = o.OrderID
-where totalprice > 1000;
+group by CustomerNo
+having TP > 1000;
