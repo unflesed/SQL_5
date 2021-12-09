35eee066a215680e5490cff444380d60e8b272bc
Anatoliy Morozov <anatoly.marozaw@gmail.com>
2021-12-09 11:09:17 +0300

    modified additional

--- additional.sql
+++ additional.sql
@@ -6,14 +6,14 @@ from orders as o
 join orderdetails as od on od.orderID = o.OrderID
 Join customers as c on o.CustomerNo = c.customerNo
 Join employees as e on o.EmployeeID = e.EmployeeID
-group by customers
+group by employees
 having TP > 1000;
 
 -- Вложенные запросы
-select 	(select FName from customers as c where c.CustomerNo = o.CustomerNo) as Customer,
-		(select FName from employees as e where o.EmployeeID = e.EmployeeID) as Employee,
+select 	(select FName from customers as c where c.CustomerNo = o.CustomerNo) as Customers,
+		(select FName from employees as e where o.EmployeeID = e.EmployeeID) as Employees,
         sum(TotalPrice) as TP
 from orders as o
 join orderdetails as od on od.orderID = o.OrderID
-group by CustomerNo
+group by EmployeeID
 having TP > 1000;
